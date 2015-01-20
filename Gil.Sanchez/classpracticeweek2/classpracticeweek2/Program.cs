cafb74e56b956f38801da3169af8200e7d5e04b0
gilsan11 <206gil@gmail.com>
2015-01-20 00:24:42 -0800

    homework2 yay + extra work

--- Gil.Sanchez/classpracticeweek2/classpracticeweek2/Program.cs
+++ Gil.Sanchez/classpracticeweek2/classpracticeweek2/Program.cs
@@ -4,15 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace homework1._1
+namespace classpracticeweek2
 {
     class Program
     {
         static void Main(string[] args)
+        private int countOfStudent = 10;
+ 
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
         }
-
     }
 }
---
+++ Gil.Sanchez/homework2/Helloworld/Helloworld/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Helloworld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, World!");
+            
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(new Uri(comboBox1.SelectedItem.ToString()));
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void goForwardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void goBackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            webBrowser1.GoHome();
+        }
+    }
+}
