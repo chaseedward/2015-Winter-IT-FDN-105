330f3166e2fe24129c7cd93f91276e5c13adaeee
evernon9681 <veacomp@hotmail.com>
2015-01-20 20:55:30 -0800

    Updated the formating of the numbers.

--- eric.vernon/Homework3/Homework3/Program.cs
+++ eric.vernon/Homework3/Homework3/Program.cs
@@ -15,7 +15,7 @@ namespace Homework3
                 //create 5 rows of entries              
                 for(int j = 1; j < 6; j++)
                 {
-                    Console.Write("{0,5}", j * i);
+                    Console.Write("{0,-5}", j * i);
                     //print row with 5 spaces in between entries
                     //each entry equals j * i
                 }
