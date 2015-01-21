3d4036aecd5d52582db912a9155ca192b48be25b
kalaj <kala.a.jones@gmail.com>
2015-01-20 20:38:19 -0800

    Form update

--- Kalaj/Homework2/WindowsApplication1/HelloWorld.vb
+++ Kalaj/Homework2/WindowsApplication1/HelloWorld.vb
@@ -7,4 +7,20 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Hello(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hello, " + TextBox1.Text + "!")
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
 End Class
