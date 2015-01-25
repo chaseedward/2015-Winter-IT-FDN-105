522a81d95c8d9a03b71ade4f9f59afbc8dcd0f49
danfitz <fitz2500@usa.net>
2015-01-25 10:21:05 -0800

    Test

---
+++ Dan.Fitzgerald/Test 2/Test 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int suma = 0;
+            int a;
+           
+
+            for (a = 1; a < 6; a++)
+            {
+                suma = 1 + a;
+
+                if (suma > 6)
+                {
+                    break;
+                }
+            }
+            
+            
+
+           
+
+                Console.WriteLine("{0}",a);
+        }
+
+                Console.ReadLine();
+
+    }
+}
