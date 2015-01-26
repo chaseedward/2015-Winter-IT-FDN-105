87b8644b670ad6626fd9abc1f690fa0639c5593a
vtbert <vtbert@yahoo.com>
2015-01-25 16:54:38 -0800

    option 2

--- Chase.Hu/Homework3/MultiplicationTable/Program.cs
+++ Chase.Hu/Homework3/MultiplicationTable/Program.cs
@@ -12,15 +12,15 @@ namespace MultiplicationTable
         {
             for (int i = 1; i < 6; i++)
             {
-                Console.Write("{0,5}", i);
+               // Console.Write("{0,5}", i);
 
                 for (int j = 1; j < 2; j++)
                 {
-                    Console.Write("{0,5}", i*2);
-                    Console.Write("{0,5}", i*3);
-                    Console.Write("{0,5}", i*4);
-                    Console.Write("{0,5}", i*5);
-                    Console.WriteLine(); 
+                    //Console.Write("{0,5}", i*2);
+                    //Console.Write("{0,5}", i*3);
+                    //Console.Write("{0,5}", i*4);
+                    //Console.Write("{0,5}", i*5);
+                    //Console.WriteLine(); 
                 }
             }
             Console.ReadLine();
