0ac00e4a30e10740fb289bbc9d1272ebd9828195
johnperez <jperez514@gmail.com>
2015-01-26 23:58:47 -0800

    Assignment 3

---
+++ JohnPerez/Assignment3/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int column = 1; column < 6; column++) //sets up columns
+            {
+                for (int row = 1; row < 6; row++) //sets up rows
+                { Console.Write("{0,-3}", row * column); } // creates table by multiplying row and column with spacing
+                Console.WriteLine();
+            }
+            
+            Console.ReadLine();
+        }
+        
+    }
+  
+}
