024197279ddc89ddb2f86fdb408105a95922f5ea
johnperez <jperez514@gmail.com>
2015-01-27 17:47:38 -0800

    Assignment 3

--- JohnPerez/Assignment3/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ JohnPerez/Assignment3/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleApplication1
             {
                 for (int row = 1; row < 6; row++) //sets up rows
                 { Console.Write("{0,-3}", row * column); } // creates table by multiplying row and column with spacing
-                Console.WriteLine();
+                Console.WriteLine(); //writes line
             }
             
             Console.ReadLine();
