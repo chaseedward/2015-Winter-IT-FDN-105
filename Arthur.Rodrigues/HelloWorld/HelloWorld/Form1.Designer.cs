d9e20fb07fde081d78646135b5ba39b02b8dc968
unknown <Arthur@XENOPHON.com>
2015-01-27 21:02:00 -0800

    01/27/2015: HW2 uploaded. **A. Rodrigues

---
+++ Arthur.Rodrigues/HelloWorld/HelloWorld/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.greetMeButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.inputNameBox = new System.Windows.Forms.TextBox();
+            this.greetingOutput = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // greetMeButton
+            // 
+            this.greetMeButton.Location = new System.Drawing.Point(42, 21);
+            this.greetMeButton.Name = "greetMeButton";
+            this.greetMeButton.Size = new System.Drawing.Size(114, 33);
+            this.greetMeButton.TabIndex = 0;
+            this.greetMeButton.Text = "Greet Me!";
+            this.greetMeButton.UseVisualStyleBackColor = true;
+            this.greetMeButton.Click += new System.EventHandler(this.greetMeButton_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(39, 63);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(49, 17);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name:";
+            // 
+            // inputNameBox
+            // 
+            this.inputNameBox.Location = new System.Drawing.Point(94, 60);
+            this.inputNameBox.Name = "inputNameBox";
+            this.inputNameBox.Size = new System.Drawing.Size(100, 22);
+            this.inputNameBox.TabIndex = 2;
+            // 
+            // greetingOutput
+            // 
+            this.greetingOutput.AutoSize = true;
+            this.greetingOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greetingOutput.Location = new System.Drawing.Point(6, 93);
+            this.greetingOutput.Name = "greetingOutput";
+            this.greetingOutput.Size = new System.Drawing.Size(0, 38);
+            this.greetingOutput.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.greetMeButton);
+            this.groupBox1.Controls.Add(this.greetingOutput);
+            this.groupBox1.Controls.Add(this.inputNameBox);
+            this.groupBox1.Controls.Add(this.nameLabel);
+            this.groupBox1.Location = new System.Drawing.Point(40, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(397, 197);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Part 1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(444, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(695, 196);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Part 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add uno mas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 253);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button greetMeButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox inputNameBox;
+        private System.Windows.Forms.Label greetingOutput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
