b7c364507db1fef8cf87cdb9df81e4cb49468520
evernon9681 <veacomp@hotmail.com>
2015-01-28 21:04:31 -0800

    Factorial problem

---
+++ eric.vernon/Factorial/Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            int factorial = 0;
+
+            for (int i = num; i <= 0; i++)
+            {
+                factorial = factorial + (num * (num - i));
+                Console.WriteLine(factorial.ToString());
+            }
+
+            Console.WriteLine("{0}!     =     {1}", num.ToString(), factorial);
+            Console.ReadLine();
+
+        }
+    }
+}
