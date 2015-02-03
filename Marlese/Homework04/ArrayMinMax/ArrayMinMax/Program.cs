6a7ef80c5ebcc71b176ee5dd5abeee8537d2e65c
Marlese <marleserose@gmail.com>
2015-02-02 17:25:04 -0800

    Homework04

--- Marlese/Homework04/ArrayMinMax/ArrayMinMax/Program.cs
+++ Marlese/Homework04/ArrayMinMax/ArrayMinMax/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ArrayMinMax
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-        }
+            int[] numbers = {5, -2, 100, 12, 4, -5};
+            {
+                Console.WriteLine("Min = " + numbers.Min());
+                Console.WriteLine("Max = " + numbers.Max());
+            }
+        } 
+             
+    
     }
 }
+
