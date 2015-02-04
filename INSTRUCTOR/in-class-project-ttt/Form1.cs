d9c78be76fbc051deb1b77228f3e3e6716832470
admin <danv@cstructor.com>
2015-02-03 20:30:51 -0800

    change tic-tac-toe to use a matrix for the buttons

--- INSTRUCTOR/in-class-project-ttt/Form1.cs
+++ INSTRUCTOR/in-class-project-ttt/Form1.cs
@@ -21,9 +21,22 @@ namespace in_class_project_ttt
         public Form1()
         {
             InitializeComponent();
+
+            grid = new Button[3,3];
+            grid[0, 0] = button1;
+            grid[0, 1] = button2;
+            grid[0, 2] = button3;
+            grid[1, 0] = button4;
+            grid[1, 1] = button5;
+            grid[1, 2] = button6;
+            grid[2, 0] = button7;
+            grid[2, 1] = button8;
+            grid[2, 2] = button9;
         }
 
-        Turn turn = Turn.X;
+        private Button[,] grid;
+
+        private Turn turn = Turn.X;
 
         private void OnButtonClick(object sender, EventArgs e)
         {
@@ -40,18 +53,47 @@ namespace in_class_project_ttt
                 turn = Turn.X;
             }
 
-            if (button1.Text == button2.Text && button2.Text == button3.Text)
+            //if (button1.Text == button2.Text && button2.Text == button3.Text)
+            //{
+            //    MessageBox.Show(button1.Text + " won");
+            //}
+            //else if (button4.Text == button5.Text && button5.Text == button6.Text)
+            //{
+            //    MessageBox.Show(button4.Text + " won");
+            //}
+            //else if (button7.Text == button8.Text && button8.Text == button9.Text)
+            //{
+            //    MessageBox.Show(button7.Text + " won");
+            //}
+
+            // Horizontals
+            var won = true;
+            var player = "";
+
+            for (var row = 0; row < grid.GetLength(0); row++)
             {
-                MessageBox.Show(button1.Text + " won");
+                player = grid[row, 0].Text;
+
+                for (var col = 1; col < grid.GetLength(1); col++)
+                {
+                    if (player != grid[row, col].Text)
+                    {
+                        won = false;
+                        break;
+                    }
+                }
             }
-            else if (button4.Text == button5.Text && button5.Text == button6.Text)
+
+            if (won)
             {
-                MessageBox.Show(button4.Text + " won");
+                MessageBox.Show(player + " won");
             }
-            else if (button7.Text == button8.Text && button8.Text == button9.Text)
-            {
-                MessageBox.Show(button7.Text + " won");
-            }
+
+            // Verticals
+            //for (cols)
+            //{
+            //    for (rows)
+            //}
         }
     }
 }
