2129a5645d7c4a4a34a93efef473161daa95e68e
dreamaddict <tcs923@gmail.com>
2015-02-04 14:34:16 -0800

    Homework 5 completed.

---
+++ Tristan.Smith/homework5/homework5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework5
+{
+    class BankAccount
+    {
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get
+            {
+                return decimal.Round(balance, 2, MidpointRounding.AwayFromZero);
+            }
+        }
+
+        public void Deposit(decimal money)
+        {
+            balance += money;
+        }
+
+        public void Withdrawal(decimal money)
+        {
+            balance -= money;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acct = new BankAccount();
+
+            acct.Deposit(200m);
+
+            acct.Withdrawal(40m);
+
+            Console.WriteLine("Balance {0:C}", acct.Balance);
+
+            Console.ReadLine();
+        }
+    }
+}
