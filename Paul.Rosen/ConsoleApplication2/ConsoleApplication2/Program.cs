54db3cd6fdd7b2073d395f1e82ed3acc46d74306
umaxcos <umaxcos@yahoo.com>
2015-02-03 19:00:20 -0800

    Homework #3

---
+++ Paul.Rosen/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int column, row;
+            Console.WriteLine("An Array Using Nested Loops:");
+            for (row = 1; row < 6; row++)
+            {
+                for (column = 1; column < 6; column++)
+                {
+                    Console.Write("{0,-3}", row*column);
+                   
+                }
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            Console.Write("Press [Enter] to Continue: ");
+            Console.ReadLine();
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("An Array Using a Single Loop:");
+            for (row = 1; row < 6; row++)
+            {
+                Console.Write("{0,-3}", row);
+                Console.Write("{0,-3}", row * 2);
+                Console.Write("{0,-3}", row * 3);
+                Console.Write("{0,-3}", row * 4);
+                Console.WriteLine("{0,-3}", row * 5);
+            }
+            Console.WriteLine(" ");
+            Console.Write("Press [Enter] to Continue: ");
+            Console.ReadLine();
+        }
+    }
+}
