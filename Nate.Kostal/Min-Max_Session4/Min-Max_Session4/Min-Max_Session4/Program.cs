f81ff67c9ee8e9b3ef9a8be5c7c94e98ff4cbe5c
natekostal <natekostal@gmail.com>
2015-02-03 18:47:13 -0800

    Homework Session #4

---
+++ Nate.Kostal/Min-Max_Session4/Min-Max_Session4/Min-Max_Session4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min_Max_Session4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myarray = new[] { 5, -2, 100, 12, 4, -5 };
+            var max = myarray[0];
+            var min = myarray[0];
+
+            foreach (var x in myarray)
+            {
+                if (x >= max)
+                {
+                    max = x;
+                }
+                if (x <= min)
+                {
+                    min = x;
+                }
+            }
+            string str = Convert.ToString(myarray);
+            Console.WriteLine("The values in the array are: "+ String.Join(", ", myarray));
+            Console.WriteLine("The Max Number is " + max);
+            Console.WriteLine("The Min Number is " + min);
+            Console.ReadLine();
+        }
+    }
+}
