7e7f332867dd44c2ccc12d46e27534caa2345784
admin <danv@cstructor.com>
2015-02-03 19:46:07 -0800

    missing teleivsion change

--- INSTRUCTOR/in-class-project-television/Program.cs
+++ INSTRUCTOR/in-class-project-television/Program.cs
@@ -1,15 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// File: tv.cs
 
-namespace in_class_project_television
+class Television
 {
-    class Program
+    private int channel = 0;
+    private int volume = 0;
+
+    public bool IsOn
     {
-        static void Main(string[] args)
+        get { return isOn; }
+        set { isOn = value; }
+    }
+
+    private bool isOn = false;
+
+    public int Volume
+    {
+        get { return volume; }
+        set
         {
+            if (value >= 0 && value <= 100)
+            {
+                volume = value;
+            }
         }
+    }
+
+    public void IncreaseVolume()
+    {
+        volume++; // this is bad
+        Volume++; // this good
+    }
+
+    public int Channel
+    {
+        get { return channel; }
+        set
+        {
+            if (value > 0 && value < 50)
+            {
+                channel = value;
+            }
+        }
+    }
+}
+
+class TestTV
+{
+    static void Main()
+    {
+        Television tv = new Television();
+
+        if (!tv.IsOn)
+        {
+            tv.IsOn = true;
+        }
+
+        tv.Channel = 3;
+
+        tv.Volume++;
+        tv.Volume = tv.Volume + 1;
+
+        tv.Volume++;
+        tv.Volume--;
+        tv.Volume--;
+
+        tv.IsOn = false;
     }
 }
