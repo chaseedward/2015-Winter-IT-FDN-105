ab9d5638b5efeb2a6bb957357670b05752fe9ab2
vtbert <vtbert@yahoo.com>
2015-02-04 22:33:03 -0800

    baseline

---
+++ robert.davey/HomeWork2/HW2_davey/HW2_davey/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW2_davey
+{
+    internal enum Turn
+    {
+        x,
+        o,
+    };
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("hello world");
+        }
+       
+    }
+}
