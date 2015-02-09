8e9876747fb465a0a18e72e98d65caf276fdb7ad
Dogboy <puzzle@gmail.com>
2015-02-09 12:54:08 -0800

    minor

--- JohnSmith/Homework05/BankAccount.cs/BankAccount.cs/Program.cs
+++ JohnSmith/Homework05/BankAccount.cs/BankAccount.cs/Program.cs
@@ -12,24 +12,24 @@ namespace BankAccount.cs
         // a public Deposit function that takes a decimal amount, 
         // and public Withdrawal function that takes a decimal amount.
         // TODO: create the balance property
-        private decimal balance;
+        private decimal _balance;
 
         // TODO: create the deposit function
         public void Deposit(decimal amount)
         {
-            balance += amount;
+            _balance += amount;
         }
 
         // TODO: create the withdrawal function
         public void Withdrawal(decimal amount)
         {
-            balance -= amount;
+            _balance -= amount;
         }
 
         // TODO:  add this to the assignment directions
         public decimal Balance()
         {
-            return balance;
+            return _balance;
         }
 
     }
