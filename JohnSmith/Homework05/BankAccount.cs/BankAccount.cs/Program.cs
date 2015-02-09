6f36255a6e587ed0b30a735873aa6f71d0121fc8
Dogboy <puzzle@gmail.com>
2015-02-09 12:49:10 -0800

    Homework 05 complete

---
+++ JohnSmith/Homework05/BankAccount.cs/BankAccount.cs/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.cs
+{
+    class BankAccount
+    {
+        // Create a BankAccount class that has a private balance field, 
+        // a public Deposit function that takes a decimal amount, 
+        // and public Withdrawal function that takes a decimal amount.
+        // TODO: create the balance property
+        private decimal balance;
+
+        // TODO: create the deposit function
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        // TODO: create the withdrawal function
+        public void Withdrawal(decimal amount)
+        {
+            balance -= amount;
+        }
+
+        // TODO:  add this to the assignment directions
+        public decimal Balance()
+        {
+            return balance;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var acct = new BankAccount();
+
+            acct.Deposit(200m);
+            acct.Withdrawal(40m);
+            
+            Console.WriteLine("Balance {0:C}", acct.Balance());
+            Console.ReadLine();
+        }
+    }
+}
