3c179d20facc7009aaf9485fa9d73d89ed6586de
gilsan11 <206gil@gmail.com>
2015-02-09 18:04:54 -0800

    Homework-5-BankAcc

---
+++ Gil.Sanchez/Homework-5-BankAccount/Homework-5-BankAccount/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Homework_5_BankAccount
+{
+    class Program
+    {
+        private const double Balance = 0;
+        private const double Deposit = 200;
+        private const double Withdraw = 40;
+
+        public double GetBalance()
+        {
+            return Deposit-Withdraw;
+        }
+
+        public double GetDeposit()
+        {
+            return Balance + Deposit;
+        }
+
+        public double GetWithdraw()
+        {
+            return Balance - Withdraw;
+        }
+
+        private static void Main(string[] args)
+
+        {
+            Program acc = new Program();
+            Console.WriteLine("Your Balance is:$ "  + acc.GetBalance  ());
+            ;
+            Console.WriteLine("Your Deposit is:  " + acc.GetDeposit());
+            Console.WriteLine("Your Withdraw is: " + acc.GetWithdraw());
+            Console.ReadKey();
+        }
+    }
+}
+
+/*static void Main()
+        {
+            var acct = new BankAccount();
+            acct.Deposit(200.00);
+            acct.Withdrawal(40.00);
+            Console.WriteLine("Balance {0:C}", acct.Balance());
+            Console.ReadLine(); */
