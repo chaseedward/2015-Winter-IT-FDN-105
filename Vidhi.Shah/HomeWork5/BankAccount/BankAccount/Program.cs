4c628e9f49e9db539ff8d6c8cc4ce30708c2eee0
vidhis <vidhimshah@gmail.com>
2015-02-10 18:01:28 -0800

    Homework 5 completed!

---
+++ Vidhi.Shah/HomeWork5/BankAccount/BankAccount/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+
+    class BankAccount
+    {
+        public Decimal Balance;
+
+        public void Withdrawal(Decimal wd)
+        {
+            Balance = Balance - wd;
+        }
+
+        public void Deposit(Decimal dd)
+        {
+            Balance = Balance + dd;
+        }
+
+        public BankAccount()
+        {
+            Balance = 160;
+        }
+    }
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acct = new BankAccount();
+
+            acct.Deposit(200m);
+
+            acct.Withdrawal(40m);
+
+            Console.WriteLine("Balance {0:C}", acct.Balance);
+
+            Console.ReadLine();
+
+        }
+    }
+}
