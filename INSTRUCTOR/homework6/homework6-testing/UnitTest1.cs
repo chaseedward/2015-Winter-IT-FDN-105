5ecf8ef28bcdff6182237652271225cea887053b
admin <danv@cstructor.com>
2015-02-10 16:14:47 -0800

    Add homework6

---
+++ INSTRUCTOR/homework6/homework6-testing/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using homework6;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace homework6_testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private MyMath myMath;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            myMath = new MyMath();
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            var result = myMath.Add(16, 24);
+            Assert.AreEqual(40, result);
+        }
+
+        [TestMethod]
+        public void TestSubtract()
+        {
+            var result = myMath.Subtract(16, 24);
+            Assert.AreEqual(-8, result);
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            var result = MyMath.Multiply(16, 24);
+            Assert.AreEqual(384, result);
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            var result = MyMath.Divide(16, 24);
+            Assert.AreEqual(.667, result);
+        }
+    }
+}
