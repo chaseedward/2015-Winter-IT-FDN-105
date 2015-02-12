69e55e022f6a5314009a79b0287065eceaa0abea
tgadgil <gadgil.trupti@gmail.com>
2015-02-11 21:20:58 -0800

    Homework4_TG

---
+++ Trupti.Gadgil/Homework4/Homework4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5, -2, 100, 12, 4, -5 };
+            int max = numbers[0];
+            int min = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+                if (min > numbers[i])
+                {
+                    min = numbers[i];
+                }
+            }
+            Console.WriteLine("Min = {0} \n\rMax = {1}", min, max);
+            Console.ReadLine();
+        }
+    }
+}
