8ffa6ceeabb2c2f07e087016e61c12aca0598df8
seacor <seacor@gmail.com>
2015-02-13 15:10:49 -0800

    Homework 6 complete

---
+++ Sean.Corbin/homework6/homework6/MyMath.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace homework6
+{
+    public class MyMath
+    {
+        public decimal Result=0;
+        public decimal Add(decimal n1, decimal n2)
+        {
+            Result=n1+n2;
+            return Result;
+        }
+        public decimal Subtract(decimal n1, decimal n2)
+        {
+            Result=n1-n2;
+            return Result;
+        }
+        public decimal Multiply(decimal n1, decimal n2)
+        {
+            Result=n1*n2;
+            return Result;
+        }
+        public decimal Divide(decimal n1, decimal n2)
+        {
+            Result=n1 / n2;
+            return Result;
+        }
+    }
+}
