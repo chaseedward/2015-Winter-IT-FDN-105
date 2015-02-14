d63638ccec299cfeb7a425f3f9cfc7f07364075c
mkundid <mkundid@gmail.com>
2015-02-14 08:54:37 -0800

    Homework 6 Data

---
+++ Michael.Kundid/Homework5/BankAccocuntClass/BankAccocuntClass/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccocuntClass
+{
+    class BankAccount
+    {
+        //TODO: create the balance property
+        //TODO: Create the deposit function
+        //TODO: Create the Withdrawal Function
+        public decimal Balance = 0;
+        private decimal withdrawal = 0;
+        private decimal deposit = 0;
+
+        public void Deposit(decimal @decimal)
+        {
+            
+        }
+
+        public void Withdrawal(decimal @decimal)
+        {
+            
+        }
+    }
+
+    class Test
+    {
+        static void Main()
+        {
+            var acct = new BankAccount();
+
+            acct.Deposit(200m);
+            acct.Withdrawal(40m);
+
+            Console.WriteLine("Balance {0:C}",acct.Balance);
+        }
+    }
+
+}
+
