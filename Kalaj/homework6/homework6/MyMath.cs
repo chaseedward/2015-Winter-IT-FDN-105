a55369e741967cec07ba3d0902c1eca9d6d612cb
kalaj <kala.a.jones@gmail.com>
2015-02-14 18:08:18 -0800

    MyMath base

---
+++ Kalaj/homework6/homework6/MyMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace homework6
+{
+    public class MyMath
+    {
+        public MyMath()
+        {
+            //
+            //TODO: Make make the unit tests pass. The class should be
+            //TODO: Write a test to verifiy that fibonacci.Calc(9) is 13.
+            //
+        }
+    }
+}
