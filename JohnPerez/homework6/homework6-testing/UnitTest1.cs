f51ca3f1811e591a7eba16f77e574dcb86d6b143
johnperez <jperez514@gmail.com>
2015-02-16 23:46:05 -0800

    Assignment 6

---
+++ JohnPerez/homework6/homework6-testing/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace homework6_testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = homework6.fibonacci.Calc(9);
+            Assert.AreEqual(21, result); //9th fibonacci is 21.
+        }
+    }
+}
